3e7d736c29c7b9c069a62924ca166ef59460906a
Jim Tufts <jim@example.com>
2025-02-06 18:21:11 -0600

    Update setup.py to better handle OSX

--- python/setup.py
+++ python/setup.py
@@ -13,21 +13,22 @@ extra_compile_args = ['-std=c++11']
 extra_link_args = []
 
 if platform.system() == 'Darwin':
-    extra_compile_args += ['-stdlib=libc++', '-mmacosx-version-min=10.7']
-    extra_link_args += ['-stdlib=libc++', '-mmacosx-version-min=10.7', '-Wl', '-rpath', openmm_dir+'/lib']
+    extra_compile_args += ['-stdlib=libc++', '-mmacosx-version-min=10.13']
+    extra_link_args += ['-stdlib=libc++', '-mmacosx-version-min=10.13', '-Wl', '-rpath', openmm_dir+'/lib']
 
 extension = Extension(name='_gridforceplugin',
-                      sources=['GridForcePluginWrapper.cpp'],
-                      libraries=['OpenMM', 'OpenMMGridForce'],
-                      include_dirs=[os.path.join(openmm_dir, 'include'), gridforceplugin_header_dir],
-                      library_dirs=[os.path.join(openmm_dir, 'lib'), gridforceplugin_library_dir],
-                      extra_compile_args=extra_compile_args,
-                      extra_link_args=extra_link_args
-                     )
+                     sources=['GridForcePluginWrapper.cpp'],
+                     libraries=['OpenMM', 'OpenMMGridForce'],
+                     include_dirs=[os.path.join(openmm_dir, 'include'), gridforceplugin_header_dir],
+                     library_dirs=[os.path.join(openmm_dir, 'lib'), gridforceplugin_library_dir],
+                     extra_compile_args=extra_compile_args,
+                     extra_link_args=extra_link_args
+                    )
 
 setup(name='gridforceplugin',
       version='0.1',
       py_modules=['gridforceplugin'],
       ext_modules=[extension],
+      install_requires=[], 
+      zip_safe=False
 )
-
