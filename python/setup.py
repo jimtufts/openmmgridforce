90c459bcd64c514ede5873c13191beb8a756bca1
jimtufts <77411026+jimtufts@users.noreply.github.com>
2026-06-22 20:36:19 -0500

    Bump version to 0.8.1

--- python/setup.py
+++ python/setup.py
@@ -51,7 +51,7 @@ extension = Extension(name='_gridforceplugin',
                     )
 
 setup(name='gridforceplugin',
-      version='0.8.0',
+      version='0.8.1',
       py_modules=['gridforceplugin', 'grid_io', 'nc_converter'],
       ext_modules=[extension],
       install_requires=[],
